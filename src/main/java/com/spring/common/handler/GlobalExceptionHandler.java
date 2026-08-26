70820f1952b753996d33341544dd3020915a9064
Diego Camargo <dabrahamcc@gmail.com>
2026-08-26 12:09:04 -0600

    - Added Common AutoConfiguration for detect the project, it doesn't matter the package name - News ExceptionHandler - Added path name in ApiResponseDTO

--- src/main/java/com/spring/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/spring/common/handler/GlobalExceptionHandler.java
@@ -2,12 +2,17 @@ package com.spring.common.handler;
 
 import com.spring.common.dto.ApiResponseDTO;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.NoSuchElementException;
 
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,10 +22,47 @@ public class GlobalExceptionHandler {
 			NoSuchElementException exception
 			) {
 		return ResponseEntity
-				.status(HttpStatus.NOT_FOUND)
-				.body(ApiResponseDTO.error("Resource not found: " + exception.getLocalizedMessage()));
+				.status(HttpStatus.NOT_FOUND) // Error 404
+				.body(ApiResponseDTO.error("Resource not found: " + exception.getLocalizedMessage(), "/path"));
 	}
 	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<ApiResponseDTO<Object>> missingServletRequestParameterExceptionHable(
+			MissingServletRequestParameterException exception,
+			HttpServletRequest request
+			) {
+		
+		String requestPath = "[" + request.getMethod() + "]" + request.getRequestURI();
+		
+		return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST) // Error 400
+				.body(ApiResponseDTO.error(exception.getLocalizedMessage(), requestPath));
+	}
 	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ApiResponseDTO<Object>> methodArgumentTypeMismatchException(
+			MethodArgumentTypeMismatchException exception,
+			HttpServletRequest request
+			) {
+		
+		String requestPath = "[" + request.getMethod() + "]" + request.getRequestURI();
+		
+		return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST) // Error 400
+				.body(ApiResponseDTO.error(exception.getLocalizedMessage(), requestPath));
+	}
+
+	@ExceptionHandler(InvalidDataAccessApiUsageException.class)
+	public ResponseEntity<ApiResponseDTO<Object>> invalidDataAccessApiUsageException(
+		MethodArgumentTypeMismatchException exception,
+		HttpServletRequest request
+		) {
+	
+	String requestPath = "[" + request.getMethod() + "]" + request.getRequestURI();
+	
+	return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR) // Error 500
+			.body(ApiResponseDTO.error(exception.getLocalizedMessage(), requestPath));
+	}
 	
 }
